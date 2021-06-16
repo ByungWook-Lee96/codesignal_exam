d2bb1ece492c749eafca7a467983c0c1bee080cb
DESKTOP-3GBPLU2\병욱 <dlquddnr414@naver.com>
2021-06-16 19:20:55 +0900

    2021-06-16

---
+++ exam2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exam2
+{
+    // Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] inputArray = { 3, 6, -2, -5, 7, 3 };
+        }
+        static int adjacentElementsProduct(int[] inputArray)
+        {
+            int max = -1001;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                if (inputArray[i] * inputArray[i + 1] >= max)
+                {
+                    max = inputArray[i] * inputArray[i + 1];
+                }
+            }
+            return max;
+
+        }
+    }
+}
