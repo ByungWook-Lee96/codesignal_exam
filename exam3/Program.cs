6d1c96f7fdf1aaacc29cbc5659a4d8cb056273e5
DESKTOP-3GBPLU2\병욱 <dlquddnr414@naver.com>
2021-06-16 19:54:31 +0900

    2021-06-16

---
+++ exam3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exam3
+{
+    // n=1 1, n=2 5, n=3 13, n=4 25,...
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            Console.WriteLine(shapeArea(n));
+        }
+        static int shapeArea(int n)
+        {
+            return 2 * n * n - 2 * n + 1;
+        }
+    }
+}
