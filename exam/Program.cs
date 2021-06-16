308917187d6e70549b1dd4507b05fe5ca1a6c0d9
DESKTOP-3GBPLU2\병욱 <dlquddnr414@naver.com>
2021-06-16 19:05:05 +0900

    초기 커밋.

---
+++ exam/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace exam
+{
+    // Given the string, check if it is a palindrome.
+    // 양쪽 짝 맞추기
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inputString = "aabaa";
+            bool b = checkPalindrome(inputString);
+            Console.WriteLine(b);
+            bool a = checkPalindrome_easy(inputString);
+            Console.WriteLine(a);
+        }
+        static bool checkPalindrome(string inputString)
+        {
+            char[] ch = inputString.ToCharArray();
+            bool b = true;
+            for (int i = 0; i < inputString.Length / 2; i++)
+            {
+                if (ch[i] == ch[inputString.Length - i - 1])
+                {
+                    continue;
+                }
+                else
+                {
+                    b = false;
+                    break;
+                }
+            }
+            return b;
+        }
+        static bool checkPalindrome_easy(string inputString)
+        {
+            return inputString.SequenceEqual(inputString.Reverse());
+        }
+    }
+    
+}
